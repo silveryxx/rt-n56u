312a49cfb0ce3ad0e012963e8abaac56fdacefd3
silveryxx <67977075+silveryxx@users.noreply.github.com>
2021-03-13 01:26:07 +0800

    Update ralink-flash-map.h

--- trunk/linux-3.4.x/drivers/mtd/ralink/ralink-flash-map.h
+++ trunk/linux-3.4.x/drivers/mtd/ralink/ralink-flash-map.h
@@ -1,98 +1,115 @@
-#ifndef __RALINK_FLASH_MAP_H__
-#define __RALINK_FLASH_MAP_H__
+#ifndef __RALINK_NAND_MAP_H__
+#define __RALINK_NAND_MAP_H__
 
-#define MTD_BOOT_PART_SIZE		0x00030000	/* 192K */
-#define MTD_CONFIG_PART_SIZE		0x00010000	/*  64K */
-#define MTD_FACTORY_PART_SIZE		0x00010000	/*  64K */
-#if defined (CONFIG_MTD_STORE_PART_SIZ) && (CONFIG_MTD_STORE_PART_SIZ >= 0x10000)
-#define MTD_STORE_PART_SIZE		CONFIG_MTD_STORE_PART_SIZ
+#define NAND_MTD_BOOT_PART_SIZE		CONFIG_MTD_BOOTLOADER_PART_SIZ
+#define NAND_MTD_BOOTENV_PART_SIZE	CONFIG_MTD_BOOTENV_PART_SIZ
+#define NAND_MTD_FACTORY_PART_SIZE	CONFIG_MTD_FACTORY_PART_SIZ
+#define NAND_MTD_KERNEL_PART_SIZE	CONFIG_MTD_FIRMWARE_PART_SIZ
+#define NAND_MTD_CONFIG_PART_SIZE	CONFIG_MTD_CONFIG_PART_SIZ
+#define NAND_MTD_STORE_PART_SIZE	CONFIG_MTD_STORE_PART_SIZ
+
+#if defined (CONFIG_MTD_NETGEAR_LAYOUT)
+#define NAND_MTD_FACTORY_OFFSET		CONFIG_MTD_FACTORY_OFFSET
+#define NAND_MTD_KERNEL_PART_IDX	2
+#define NAND_MTD_KERNEL_PART_OFFSET	(NAND_MTD_BOOT_PART_SIZE + NAND_MTD_BOOTENV_PART_SIZE)
+#define NAND_MTD_RWFS_PART_OFFSET	CONFIG_MTD_RWFS_OFFSET
 #else
-#define MTD_STORE_PART_SIZE		0x00010000	/*  64K */
-#endif
-
-#if defined (CONFIG_RT2880_FLASH_4M)
-#define IMAGE1_SIZE			0x00400000
-#elif defined (CONFIG_RT2880_FLASH_8M)
-#define IMAGE1_SIZE			0x00800000
-#elif defined (CONFIG_RT2880_FLASH_16M)
-#define IMAGE1_SIZE			0x01000000
-#elif defined (CONFIG_RT2880_FLASH_32M)
-#define IMAGE1_SIZE			0x02000000
+#if defined (CONFIG_MTD_CONFIG_PART_BELOW)
+#define NAND_MTD_KERNEL_PART_IDX	3
+#define NAND_MTD_KERNEL_PART_OFFSET	(NAND_MTD_BOOT_PART_SIZE + NAND_MTD_BOOTENV_PART_SIZE + NAND_MTD_FACTORY_PART_SIZE)
+#define NAND_MTD_RWFS_PART_OFFSET	(NAND_MTD_KERNEL_PART_OFFSET + NAND_MTD_KERNEL_PART_SIZE + NAND_MTD_CONFIG_PART_SIZE + NAND_MTD_STORE_PART_SIZE)
 #else
-#define IMAGE1_SIZE			CONFIG_RT2880_MTD_PHYSMAP_LEN
+#define NAND_MTD_KERNEL_PART_IDX	4
+#define NAND_MTD_KERNEL_PART_OFFSET	(NAND_MTD_BOOT_PART_SIZE + NAND_MTD_BOOTENV_PART_SIZE + NAND_MTD_CONFIG_PART_SIZE + NAND_MTD_FACTORY_PART_SIZE)
+#define NAND_MTD_RWFS_PART_OFFSET	(NAND_MTD_KERNEL_PART_OFFSET + NAND_MTD_KERNEL_PART_SIZE + NAND_MTD_STORE_PART_SIZE)
 #endif
-
-#define MTD_KERNEL_PART_OFFSET		(MTD_BOOT_PART_SIZE + MTD_CONFIG_PART_SIZE + MTD_FACTORY_PART_SIZE)
+#endif
 
 #if defined (CONFIG_RT2880_ROOTFS_IN_FLASH)
-#define MTD_STORE_PART_IDX		5
-#define MTD_FWSTUB_PART_IDX		6
+#define NAND_MTD_ROOTFS_PART_IDX	(NAND_MTD_KERNEL_PART_IDX + 1)
+#define NAND_MTD_RWFS_PART_IDX		7
 #else
-#define MTD_STORE_PART_IDX		4
-#define MTD_FWSTUB_PART_IDX		5
+#define NAND_MTD_RWFS_PART_IDX		6
 #endif
 
-/* NOR, SPI partitions (Ralink/MTK reference + mod) */
 static struct mtd_partition rt2880_partitions[] = {
 	{
-		name:   "Bootloader",		/* mtdblock0 */
-		size:   MTD_BOOT_PART_SIZE,	/* 192K */
+		name:   "Bootloader",			/* mtdblock0 */
+		size:   NAND_MTD_BOOT_PART_SIZE,	/* 512K */
 		offset: 0,
 	}, {
-		name:   "Config",		/* mtdblock1 */
-		size:   MTD_CONFIG_PART_SIZE,	/* 64K */
+		name:   "BootEnv",			/* mtdblock1 */
+		size:   NAND_MTD_BOOTENV_PART_SIZE,	/* 256K */
 		offset: MTDPART_OFS_APPEND,
+#if (!defined (CONFIG_MTD_CONFIG_PART_BELOW) && !defined (CONFIG_MTD_NETGEAR_LAYOUT))
 	}, {
-		name:   "Factory",		/* mtdblock2 */
-		size:   MTD_FACTORY_PART_SIZE,	/* 64K */
+		name:   "Config",			/* mtdblock2 */
+		size:   NAND_MTD_CONFIG_PART_SIZE,	/* 1024K */
 		offset: MTDPART_OFS_APPEND,
+#endif
+#if !defined (CONFIG_MTD_NETGEAR_LAYOUT)
 	}, {
-		name:   "Kernel",		/* mtdblock3 */
-		size:   0,			/* calc */
-		offset: MTD_KERNEL_PART_OFFSET,
-#if defined (CONFIG_RT2880_ROOTFS_IN_FLASH)
-	}, {
-		name:   "RootFS",		/* mtdblock4 */
-		size:   0,			/* calc */
+		name:   "Factory",			/* mtdblock3 */
+		size:   NAND_MTD_FACTORY_PART_SIZE,	/* 256K */
 		offset: MTDPART_OFS_APPEND,
 #endif
 	}, {
-		name:   "Storage",		/* mtdblock5 */
-		size:   MTD_STORE_PART_SIZE,	/* 64K */
-		offset: MTDPART_OFS_APPEND,
+		name:   "Kernel",			/* mtdblock4 */
+		size:   NAND_MTD_KERNEL_PART_SIZE,	/* 16M */
+		offset: NAND_MTD_KERNEL_PART_OFFSET,
+#if defined (CONFIG_RT2880_ROOTFS_IN_FLASH)
 	}, {
-		name:   "Firmware_Stub",	/* mtdblock6 */
-		size:   0,			/* calc */
-		offset: MTD_KERNEL_PART_OFFSET,
+		name:   "RootFS",			/* mtdblock5 */
+		size:   0,				/* calc */
+		offset: MTDPART_OFS_APPEND,
+#endif
+#if (defined (CONFIG_MTD_CONFIG_PART_BELOW) || defined(CONFIG_MTD_NETGEAR_LAYOUT))
+	}, {
+		name:   "Config",			/* mtdblockX */
+		size:   NAND_MTD_CONFIG_PART_SIZE,	/* 1024K */
+		offset: MTDPART_OFS_APPEND,
+#endif
+	}, {
+		name:   "Storage",			/* mtdblock6 */
+		size:   NAND_MTD_STORE_PART_SIZE,	/* 2M */
+		offset: MTDPART_OFS_APPEND,
+#if defined (CONFIG_MTD_NETGEAR_LAYOUT)
+	}, {
+		name:   "Factory",			/* mtdblockY */
+		size:   NAND_MTD_FACTORY_PART_SIZE,	/* 2M */
+		offset: NAND_MTD_FACTORY_OFFSET,
+	}, {
+		name:   "RWFS",				/* mtdblock7 */
+		size:   0,				/* calc */
+		offset: NAND_MTD_RWFS_PART_OFFSET,
+#else
+	}, {
+		name:   "RWFS",				/* mtdblock7 */
+		size:   0,				/* calc */
+		offset: MTDPART_OFS_APPEND,
+#endif
+	}, {
+		name:   "Firmware_Stub",		/* mtdblock8 */
+		size:   NAND_MTD_KERNEL_PART_SIZE,	/* Kernel+RootFS */
+		offset: NAND_MTD_KERNEL_PART_OFFSET,
 	}
 };
 
 inline void recalc_partitions(uint64_t flash_size, uint32_t kernel_size)
 {
-	/* first, calc "Storage" size (use 256K for >= 16MB flash) */
-#if !defined (CONFIG_MTD_STORE_PART_SIZ)
-	if (flash_size > 0x00800000)
-		rt2880_partitions[MTD_STORE_PART_IDX].size = 0x00040000;
-#endif
-
 	/* calc "Kernel" size */
 #if defined (CONFIG_RT2880_ROOTFS_IN_FLASH)
 #if defined (CONFIG_ROOTFS_IN_FLASH_NO_PADDING)
-	rt2880_partitions[3].size = kernel_size;
+	rt2880_partitions[NAND_MTD_KERNEL_PART_IDX].size = kernel_size;
 #else
-	rt2880_partitions[3].size = CONFIG_MTD_KERNEL_PART_SIZ;
+	rt2880_partitions[NAND_MTD_KERNEL_PART_IDX].size = CONFIG_MTD_KERNEL_PART_SIZ;
 #endif
 	/* calc "RootFS" size */
-	rt2880_partitions[4].size = flash_size - (MTD_KERNEL_PART_OFFSET + rt2880_partitions[MTD_STORE_PART_IDX].size + rt2880_partitions[3].size);
-#else
-	/* ROOTFS_IN_RAM */
-	rt2880_partitions[3].size = flash_size - (MTD_KERNEL_PART_OFFSET + rt2880_partitions[MTD_STORE_PART_IDX].size);
+	rt2880_partitions[NAND_MTD_ROOTFS_PART_IDX].size = NAND_MTD_KERNEL_PART_SIZE - rt2880_partitions[NAND_MTD_KERNEL_PART_IDX].size;
 #endif
 
-	/* calc "Firmware_Stub" size (allow crossing over "Storage" for <= 8 MB flash) */
-	rt2880_partitions[MTD_FWSTUB_PART_IDX].size = flash_size - MTD_KERNEL_PART_OFFSET;
-	if (flash_size > 0x00800000)
-		rt2880_partitions[MTD_FWSTUB_PART_IDX].size -= rt2880_partitions[MTD_STORE_PART_IDX].size;
+	/* calc "RWFS" size (UBIFS or JFFS2) */
+	rt2880_partitions[NAND_MTD_RWFS_PART_IDX].size = flash_size - NAND_MTD_RWFS_PART_OFFSET;
 }
 
 #endif
