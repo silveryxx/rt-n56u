4d3c0e5dfff3f83d5163a14a9ed05947cf007480
silveryxx <67977075+silveryxx@users.noreply.github.com>
2021-03-13 02:00:15 +0800

    Update board.h

--- trunk/configs/boards/R6800/board.h
+++ trunk/configs/boards/R6800/board.h
@@ -1,7 +1,7 @@
 /* NETGEAR-R6800 */
 
 #define BOARD_PID		"R6800"
-#define BOARD_NAME		"NETGEAR-R6800"
+#define BOARD_NAME		"R6800"
 #define BOARD_DESC		"NETGEAR R6800 Dual MT7615 Wireless Router"
 #define BOARD_VENDOR_NAME	"NETGEAR Co., Ltd."
 #define BOARD_VENDOR_URL	"http://www.netgear.com/"
